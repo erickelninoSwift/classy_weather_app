5a76a4f4e4028d747e011883f0ed4cd49e274ae1
Erick El nino <erick.tshimbombo@yahoo.com>
2023-09-14 20:48:41 +0200

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import "./App.css";
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+  handleDecrement() {
+    this.setState((current) => {
+      return {
+        counter: current.counter - 1,
+      };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((current) => {
+      return {
+        counter: current.counter + 1,
+      };
+    });
+  }
+  render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.counter);
+    return (
+      <>
+        <div>
+          <button onClick={this.handleIncrement}>+</button>
+          {date.toDateString()} ...[{this.state.counter}]
+          <button onClick={this.handleDecrement}>-</button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
